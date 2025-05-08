df38dcecfbd48f41787607d8fa2cf58877c32241
Divine <rickybanky4@gmail.com>
2025-05-08 23:56:11 +0100

    dist copy to server

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import contactRoutes from './routes/contactRoutes.js';
 import bodyParser from 'body-parser';
-import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 3000;
 
-
+app.use(express.static('dist'));
 app.use(bodyParser.json())
 app.use('/api/contacts', contactRoutes)
 console.log('Server initialized');
